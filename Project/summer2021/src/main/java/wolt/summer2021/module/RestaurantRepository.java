618ad8f95247b1659ba834da22cecfa770768d75
Taehee Lee <69889362+taeheelee90@users.noreply.github.com>
2021-01-27 23:13:04 +0200

    Delete Project directory

--- Project/summer2021/src/main/java/wolt/summer2021/module/RestaurantRepository.java
+++
@@ -1,9 +0,0 @@
-package wolt.summer2021.module;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
-
-@Transactional(readOnly = true)
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
-
-}
